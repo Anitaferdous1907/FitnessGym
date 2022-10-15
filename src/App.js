f76f87c4a6cc0895abffdf8047b0ee74e1accdbc
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-10-16 02:07:15 +0600

    add heading text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Heading from './components/Heading/Heading';
 
 function App() {
   return (
     <div>
      <Header></Header>
+     <Heading></Heading>
     </div>
   );
 }
---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Heading.css'
+
+const Heading = () => {
+    return (
+        <div>
+            <h2>Choose your favorite workout</h2>
+        </div>
+    );
+};
+
+export default Heading;
