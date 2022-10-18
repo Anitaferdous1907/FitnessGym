5384e72f0eb1b6f39e1ae5115c05627d90b26e0c
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-10-18 19:06:06 +0600

    show array length in consol

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Gym from '../Gym/Gym';
 import './Activity.css'
 
 const Activity = () => {
+    const [gym,setGym] = useState([]);
+
+    useEffect( ()=>{
+        fetch('./gym.json')
+        .then(res=> res.json())
+        .then(data => setGym(data))
+    },[])
     return (
         <div className='activity-container'>
            <div className="gym-container">
-            <h2>This is gym activites</h2>
+            {
+                gym.map(gymActivity =><Gym key={gym.id}></Gym>)
+            }
            </div>
            <div className="activity-details">
            <h2>This is activities details</h2> 
---
+++ src/components/Gym/Gym.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gym = () => {
+    return (
+        <div>
+           <h1>Fitness Activity</h1> 
+        </div>
+    );
+};
+
+export default Gym;
